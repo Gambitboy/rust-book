fc9cbb7b8c76221505aebeff67e8885490658080
Keaton Roux <keaton@codehesion.co.za>
2023-05-13 12:24:34 +0200

    Variables

---
+++ variables/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of x is: {THREE_HOURS_IN_SECONDS}");
+
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("The value of space is: {spaces}");
+}
