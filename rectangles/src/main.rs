9602795cf96d7bfbb6c32454ecb18c3b392e0340
Keaton Roux <keaton@codehesionc.o.za>
2023-05-28 11:39:53 +0200

    Struct Impl and methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,11 +4,37 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            height: size,
+            width: size,
+        }
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
         width: dbg!(30 * scale), // dbg! prints out during run time to the std error stream, includes file and line number
         height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 25,
     };
 
     dbg!(&rect1);
@@ -17,10 +43,17 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(&rect1),
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    if rect1.can_hold(&rect2) {
+        println!("The first rectangle can hold the second");
+    }
+
+    let sqr = Rectangle::square(32);
+    println!("sqr is {:#?}", sqr);
 }
